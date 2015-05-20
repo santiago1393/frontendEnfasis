f55e3b31ac73d2edb8ce042d3e4d073a09e52425
Santiago Castaño <santi1393@hotmail.com>
2015-05-20 02:01:25 -0500

    List, detail and delete funcionando.. Faltan bonificaciones y algo mas de estilos

--- client/app/busqueda/busqueda.controller.js
+++ client/app/busqueda/busqueda.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('homecenterApp')
   .controller('BusquedaCtrl', function ($scope,$http) {
-
     $scope.busqueda = function () {
       $http.get('http://localhost:8088/practica/maquinas/listBusqueda?busqueda='+$scope.terminoBusqueda)
         .success(function (result) {
